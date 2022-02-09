83863729166343b45cfa320f30a05af624b97892
Jdance <Jdance@DESKTOP-H0JESVK>
2022-02-09 16:38:13 -0500

    Add project files.

---
+++ KillHeal.cs
@@ -0,0 +1,36 @@
+﻿using Rocket.Core.Logging;
+using Rocket.Core.Plugins;
+using Rocket.Unturned.Events;
+using Rocket.Unturned.Player;
+using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+
+namespace KillHeal
+{
+    public class KillHeal : RocketPlugin<KillHealConfiguration>
+    {
+        protected override void Load()
+        {
+            Logger.Log("KillHeal has loaded. Heal when you kill someone!");
+            UnturnedPlayerEvents.OnPlayerDeath += UnturnedPlayerEvents_OnPlayerDeath;
+        }
+
+        private void UnturnedPlayerEvents_OnPlayerDeath(Rocket.Unturned.Player.UnturnedPlayer player, SDG.Unturned.EDeathCause cause, SDG.Unturned.ELimb limb, Steamworks.CSteamID murderer)
+        {
+            if (murderer != Steamworks.CSteamID.Nil)
+            {
+                UnturnedPlayer mplayer = UnturnedPlayer.FromCSteamID(murderer);
+                mplayer.Heal(Configuration.Instance.amount);
+            }
+        }
+
+        protected override void Unload()
+        {
+            Logger.Log("KillHeal has unloaded. Heal when you kill someone!");
+            UnturnedPlayerEvents.OnPlayerDeath -= UnturnedPlayerEvents_OnPlayerDeath;
+        }
+    }
+}
---
+++ KillHealConfiguration.cs
@@ -0,0 +1,18 @@
+﻿using Rocket.API;
+using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+
+namespace KillHeal
+{
+    public class KillHealConfiguration : IRocketPluginConfiguration
+    {
+        public byte amount { get; set; }
+        public void LoadDefaults()
+        {
+            amount = 10;
+        }
+    }
+}
