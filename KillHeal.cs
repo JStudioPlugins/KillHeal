8e453e0320b8acbbd3815310bf9b75d588a57a8d
Jdance <Jdance@DESKTOP-H0JESVK>
2022-02-15 19:43:48 -0500

    Fixed

--- KillHeal.cs
+++ KillHeal.cs
@@ -2,6 +2,7 @@
 using Rocket.Core.Plugins;
 using Rocket.Unturned.Events;
 using Rocket.Unturned.Player;
+using SDG.Unturned;
 using System;
 using System.Collections.Generic;
 using System.Linq;
@@ -20,7 +21,7 @@ namespace KillHeal
 
         private void UnturnedPlayerEvents_OnPlayerDeath(Rocket.Unturned.Player.UnturnedPlayer player, SDG.Unturned.EDeathCause cause, SDG.Unturned.ELimb limb, Steamworks.CSteamID murderer)
         {
-            if (murderer != Steamworks.CSteamID.Nil)
+            if (PlayerTool.getPlayer(murderer) != null)
             {
                 UnturnedPlayer mplayer = UnturnedPlayer.FromCSteamID(murderer);
                 mplayer.Heal(Configuration.Instance.amount);
